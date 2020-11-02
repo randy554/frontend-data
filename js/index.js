82249e1cf5d5b3eb9b1a80264b11967db9ef3023
randy554 <randy554@hotmail.com>
2020-11-02 20:46:01 +0100

    basic async api call

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log('JS is alive!');
+
+// Set API endpoint
+const parkeerGaragesEndpoint    = `https://opendata.rdw.nl/resource/t5pc-eb34.json`;
+
+// Perform API call
+getData(parkeerGaragesEndpoint);
+
+// Get the data from the API endpoint
+async function getData(apiEndpoint) {
+
+    // Perform a call to the API, this will be paused until completion
+    const response      = await fetch(apiEndpoint);
+
+    // Wait for the JSON response
+    const data          = await response.json();
+
+    console.log(data);
+
+}
