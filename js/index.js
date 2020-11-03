42db097b1bc9c23817a5a0b2406a41ae665ee273
randy554 <randy554@hotmail.com>
2020-11-03 14:04:05 +0100

    upload images

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ console.log('JS is alive!');
 // Set API endpoint
 const parkeerGaragesEndpoint    = `https://opendata.rdw.nl/resource/t5pc-eb34.json`;
 
+// Local parkeer garage dataset
+// const parkeerGaragesEndpoint    =   parkeerGaragesJSON;
+
 // Perform API call
 getData(parkeerGaragesEndpoint);
 
@@ -11,10 +14,10 @@ async function getData(apiEndpoint) {
 
     // Perform a call to the API, this will be paused until completion
     const response      = await fetch(apiEndpoint);
+    console.log("Response: ", response);
 
     // Wait for the JSON response
     const data          = await response.json();
-
     console.log(data);
 
 }
