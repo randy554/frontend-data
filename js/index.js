06f1e05a771e58bb6a1ba79d001c3b952e97f62b
randy554 <randy554@hotmail.com>
2020-11-19 11:00:16 +0100

    detect undefined from accesPoints

--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
-console.log('JS is alive!');
-
 // Set API endpoint
-const parkeerGaragesEndpoint    = `https://opendata.rdw.nl/resource/t5pc-eb34.json`;
-
-// Local parkeer garage dataset
-// const parkeerGaragesEndpoint    =   parkeerGaragesJSON;
+// const parkeerGaragesEndpoint    = `https://npropendata.rdw.nl/parkingdata/v2/`;
+const parkeerGaragesEndpoint    = 'https://raw.githubusercontent.com/SharonV33/frontend-data/main/data/parkeergarages_1000.json';
 
 // Perform API call
-getData(parkeerGaragesEndpoint);
+getData(parkeerGaragesEndpoint).then(RDWData => {
+
+     console.log("MY RDW DATA: ", RDWData);
+     // console.log("MY RDW DATA: ", RDWData[0]);
+    // console.log("THIS IS IT: ", RDWData[0].parkingFacilityInformation.name);
+
+    // console.log('City: ', RDWData[0].parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
+    const listCities = getCities(RDWData);
+
+
+});
 
 // Get the data from the API endpoint
 async function getData(apiEndpoint) {
 
     // Perform a call to the API, this will be paused until completion
-    const response      = await fetch(apiEndpoint);
+    const response = await fetch(apiEndpoint);
     console.log("Response: ", response);
 
     // Wait for the JSON response
-    const data          = await response.json();
-    console.log(data);
+    const data = await response.json();
+
+    return data;
+}
+
+function getCities(data) {
+
+    const listCities = data.map( (value, index) => {
+
+        if (typeof value.parkingFacilityInformation.accessPoints[0] == 'undefined'){
+            return null;
+        }
+        console.log("alle data hier nog: " + index + " - " + value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
+    });
+
 
 }
