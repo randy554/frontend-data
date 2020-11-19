9b835375494b59ddd07d470be8fc20998f14fc02
randy554 <randy554@hotmail.com>
2020-11-19 16:13:45 +0100

    detect null chargingpoints en list charging points

--- js/index.js
+++ js/index.js
@@ -8,10 +8,14 @@ getData(parkeerGaragesEndpoint).then(RDWData => {
      console.log("MY RDW DATA: ", RDWData);
      // console.log("MY RDW DATA: ", RDWData[0]);
     // console.log("THIS IS IT: ", RDWData[0].parkingFacilityInformation.name);
-
     // console.log('City: ', RDWData[0].parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
+
+    // console.log("Car charging spot: ", RDWData[0].parkingFacilityInformation.specifications[0].chargingPointCapacity);
+
     const listCities = getCities(RDWData);
-    console.log('N: ', listCities);
+    console.log('All available cities: ', listCities);
+    const listChargingCapacity = getCarChargingPoints(listCities);
+    console.log('All charging capacity from cities', listChargingCapacity);
 
 });
 
@@ -30,6 +34,8 @@ async function getData(apiEndpoint) {
 
 function getCities(data) {
 
+    let counter = 0;
+
     const listCities = data.map( (value, index) => {
 
         if ((typeof value.parkingFacilityInformation.accessPoints[0] == 'undefined') || (typeof value.parkingFacilityInformation.accessPoints[0].accessPointAddress == 'undefined') || (value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city.length === 0)){
@@ -38,10 +44,33 @@ function getCities(data) {
         // console.log("alle data hier nog: " + index + " - " + value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
         // console.log("TYPE: " + typeof value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
         // return value.parkingFacilityInformation.accessPoints[0].accessPointAddress;
+        counter++;
+        console.log("GOOD cities: ", counter);
         return value.parkingFacilityInformation;
     });
 
     return listCities;
+}
+
+function getCarChargingPoints(data) {
+
+    let counter = 0;
+
+    const listchargingPoints = data.map((value, index) => {
+
+        if((value == null) || (value.specifications[0].chargingPointCapacity == null)){
+            return null;
+        }
+        // console.log("CHARGE: ", value.specifications);
+        // console.log("Car charging spot: " + index + " - ", value.specifications[0].chargingPointCapacity);
+        counter++;
+        console.log("GOOD charging: ", counter);
+        return value;
+    });
+
+    return listchargingPoints;
+
+    
 }
 
 
