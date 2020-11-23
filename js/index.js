420d1d7ac56af953ebe315d3d9be63bee4ff1226
randy554 <randy554@hotmail.com>
2020-11-23 13:54:49 +0100

    refactored index.js
    - code flow
    - naming functions
    - rewritten function

--- js/index.js
+++ js/index.js
@@ -1,34 +1,82 @@
-// Set API endpoint
+// Set API endpoints
 const RDWApiUrl = 'https://npropendata.rdw.nl/parkingdata/v2/';
 const proxyUrl  = 'https://cors-anywhere.herokuapp.com/';
 const parkeerGaragesEndpoint    = 'https://raw.githubusercontent.com/SharonV33/frontend-data/main/data/parkeergarages_1000.json';
 
 // Perform API call
-getData(parkeerGaragesEndpoint).then(RDWData => {
+// getData(parkeerGaragesEndpoint).then(RDWData => {
+//
+//      // console.log("MY RDW DATA: ", RDWData);
+//      // console.log("MY RDW DATA: ", RDWData[0]);
+//     // console.log("THIS IS IT: ", RDWData[0].parkingFacilityInformation.name);
+//     // console.log('City: ', RDWData[0].parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
+//
+//     // console.log("Car charging spot: ", RDWData[0].parkingFacilityInformation.specifications[0].chargingPointCapacity);
+//
+//     const listCities = getCities(RDWData);
+//     // console.log('All available cities: ', listCities);
+//     const listChargingCapacity = getCarChargingPoints(listCities);
+//     // console.log('All charging capacity from cities: ', listChargingCapacity);
+//     const listPaymentMethods = getPaymentMethods(listChargingCapacity);
+//     // console.log('All payments: ', listPaymentMethods);
+//     // console.log(RDWData[0].parkingFacilityInformation.paymentMethods);
+//
+//
+// });
 
-     // console.log("MY RDW DATA: ", RDWData);
-     // console.log("MY RDW DATA: ", RDWData[0]);
-    // console.log("THIS IS IT: ", RDWData[0].parkingFacilityInformation.name);
-    // console.log('City: ', RDWData[0].parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
+const RDWAllData = NPRORDWAllData[0].ParkingFacilities;
 
-    // console.log("Car charging spot: ", RDWData[0].parkingFacilityInformation.specifications[0].chargingPointCapacity);
+launchData();
 
-    const listCities = getCities(RDWData);
-    // console.log('All available cities: ', listCities);
-    const listChargingCapacity = getCarChargingPoints(listCities);
-    // console.log('All charging capacity from cities: ', listChargingCapacity);
-    const listPaymentMethods = getPaymentMethods(listChargingCapacity);
-    // console.log('All payments: ', listPaymentMethods);
-    // console.log(RDWData[0].parkingFacilityInformation.paymentMethods);
+async function launchData() {
 
+    // Get all RDW parking data
+    // const allParkingFacilitiesData = getData(proxyUrl+RDWApiUrl);
+    const allParkingIntroData = RDWAllData;
+    console.log("All parking intro data: ", allParkingIntroData);
 
-});
+    const completeParkingData = await configureParkingIntroData(allParkingIntroData);
+    console.log("Complete parking data: ", completeParkingData);
 
-const RDWAllData = NPRORDWAllData[0].ParkingFacilities.slice(100, 10);
+    return completeParkingData;
 
-getAllRDWData(RDWAllData).then(allData => {
-    console.log("Einde: ", allData);
-});
+}
+
+// getAllRDWData(RDWAllData).then(allData => {
+//     console.log("Einde: ", allData);
+// });
+
+async function configureParkingIntroData(allParkingIntroData) {
+
+    const limit = allParkingIntroData.slice(7900, 7974);
+    // Get the unique identifiers from dataset
+    // const parkingPlaceIdentifiers = allParkingIntroData.map((parking, index) => {
+    //     return parking.identifier;
+    // });
+
+    const parkingPlaceIdentifiers = limit.map((parking, index) => {
+        return parking.identifier;
+    });
+    console.log("Unique identifiers: ", parkingPlaceIdentifiers);
+
+    // Construct path to complete parking data API endpoint
+    const baseUrl = proxyUrl + RDWApiUrl + 'static/';
+    const allParkingFacilitiesData = parkingPlaceIdentifiers.map(identifier => getData(baseUrl + identifier));
+
+    console.log("All parking facilities data: ", allParkingFacilitiesData);
+    const parkingFacilitiesDataArray = await Promise.all(allParkingFacilitiesData);
+
+    const selectionParkingFacilitiesDataset = parkingFacilitiesDataArray.map(parkingFacility => {
+        console.log("PF: ", parkingFacility);
+        return {
+            city: getCities(parkingFacility),
+            chargingPoint: getCarChargingPoints(parkingFacility),
+            payment: getPaymentMethods(parkingFacility)
+        }
+    });
+
+    return selectionParkingFacilitiesDataset;
+}
 
 
 async function getAllRDWData(allRDWData) {
@@ -71,11 +119,14 @@ async function getData(apiEndpoint) {
 
     // Perform a call to the API, this will be paused until completion
     const response = await fetch(apiEndpoint);
-    // console.log("Response: ", response);
+    console.log("Response: ", response);
+
+    // Try to delay fetch
+    setTimeout(()=> {let count = 0; count++;}, 2000);
 
     // Wait for the JSON response
     const data = await response.json();
-    console.log("GETTTData: ", data);
+    // console.log("GETTTData: ", data);
 
     return data;
 }
@@ -88,62 +139,41 @@ async function getData(apiEndpoint) {
 
 function getCities(data) {
 
-    let counter = 0;
+    console.log("In cities: ", data.parkingFacilityInformation.accessPoints);
 
-    const listCities = data.map( (value, index) => {
-
-        if ((typeof value.parkingFacilityInformation.accessPoints[0] == 'undefined') || (typeof value.parkingFacilityInformation.accessPoints[0].accessPointAddress == 'undefined') || (value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city.length === 0)){
+        if ((typeof data.parkingFacilityInformation.accessPoints == 'undefined') || (typeof data.parkingFacilityInformation.accessPoints[0] == 'undefined') || (typeof data.parkingFacilityInformation.accessPoints[0].accessPointAddress == 'undefined') || (data.parkingFacilityInformation.accessPoints[0].accessPointAddress.city.length === 0)){
             return null;
         }
-        // console.log("alle data hier nog: " + index + " - " + value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
-        // console.log("TYPE: " + typeof value.parkingFacilityInformation.accessPoints[0].accessPointAddress.city);
-        // return value.parkingFacilityInformation.accessPoints[0].accessPointAddress;
-        // counter++;
-        // console.log("GOOD cities: ", counter);
-        return value.parkingFacilityInformation;
-    });
 
-    return listCities;
+        return data.parkingFacilityInformation.accessPoints[0].accessPointAddress.city;
+
+
 }
 
 function getCarChargingPoints(data) {
 
-    let counter = 0;
+    console.log("In charging: ", data.parkingFacilityInformation.specifications);
 
-    const listchargingPoints = data.map((value, index) => {
-
-        if((value == null) || (value.specifications[0].chargingPointCapacity == null)){
+        if((data.parkingFacilityInformation == null) || (typeof data.parkingFacilityInformation.specifications == 'undefined') || (data.parkingFacilityInformation.specifications[0] == null) || (data.parkingFacilityInformation.specifications[0].chargingPointCapacity == null)){
             return null;
         }
-        // console.log("CHARGE: ", value.specifications);
-        // console.log("Car charging spot: " + index + " - ", value.specifications[0].chargingPointCapacity);
-        // counter++;
-        // console.log("GOOD charging: ", counter);
-        return value;
-    });
 
-    return listchargingPoints;
-    
+        return  data.parkingFacilityInformation.specifications[0].chargingPointCapacity;
+
 }
 
 function getPaymentMethods(data) {
 
-    let counter = 0;
+    console.log("In payment: ", data.parkingFacilityInformation.paymentMethods);
 
-    const listPaymentMethods = data.map((value, index) => {
-
-        if((value == null) || (value.paymentMethods.length === 0)){
+        if((data.parkingFacilityInformation == null) || (typeof data.parkingFacilityInformation.paymentMethods == 'undefined') || (data.parkingFacilityInformation.paymentMethods.length === 0)){
             return null;
         }
 
-        // counter++;
-        // console.log("PAYMENT: ", value.paymentMethods + " - " + counter);
-        return value;
-    });
-
-    return listPaymentMethods;
+        return data.parkingFacilityInformation.paymentMethods;
 
 }
+
 
 
 
